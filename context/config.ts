05b6dfa4e9b064f7c0b6ac670e369325dacccb54
AmirMP <122886182+AmirMP12@users.noreply.github.com>
2023-03-04 02:21:00 +0330

    Update config.ts

--- context/config.ts
+++ context/config.ts
@@ -3,7 +3,7 @@ import { Api, JsonRpc, JsSignatureProvider } from "ineryjs";
 const actor = "amirmp";
 const private_key = process.env.PRIVATE_KEY;
 const signature = new JsSignatureProvider([private_key!]);
-const node_url = "process.env.NODE_URL";
+const node_url = "https://5.144.132.50:8888";
 const json_rpc = new JsonRpc(node_url);
 
 
